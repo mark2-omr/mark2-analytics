5c0966baac6e94bb22d3ccaa5d3368c89c0522d4
Kaoru Mori <kaoru.mori@defrag.works>
2023-05-01 22:19:18 +0900

    Add analysis loading style and change Auth0 scope (#47)
    * Use latest pg
    
    * Add loading style to analysis page
    
    * Modify Auth0 scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            Rails.application.credentials.auth0.app.client_id,
            Rails.application.credentials.auth0.app.client_secret,
            Rails.application.credentials.auth0.domain,
-           callback_path: "/auth/auth0/callback",
-           scope: "openid email"
+           callback_path: '/auth/auth0/callback',
+           scope: 'openid'
 
   OmniAuth.config.on_failure =
     Proc.new { |env| OmniAuth::FailureEndpoint.new(env).redirect_to_failure }
