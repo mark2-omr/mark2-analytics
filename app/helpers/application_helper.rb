dc68a7b987975f9d27afe592dfe516f1df46d797
Kaoru Mori <kaoru.mori@defrag.works>
2023-07-06 13:51:25 +0900

    Apply before action to admin contoller (#76)
    * Update Rails from 7.0.5.1 to 7.0.6
    
    * Change double quotes to single quotes in default_meta_tags
    
    * Apply before_action to admin controller
    
    * Fix SQL for importing database

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,31 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "Mark2 Analytics",
+      site: 'Mark2 Analytics',
       title: nil,
       reverse: true,
-      separator: "|",
-      description: "",
-      keywords: "Mark2",
+      separator: '|',
+      description: '',
+      keywords: 'Mark2',
       canonical: request.original_url,
       noindex: !Rails.env.production?,
       icon: [
-        { href: image_url("favicon.svg") },
+        { href: image_url('favicon.svg') },
         {
-          href: image_url("favicon.png"),
-          rel: "apple-touch-icon",
-          sizes: "180x180",
-          type: "image/png",
+          href: image_url('favicon.png'),
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          type: 'image/png',
         },
       ],
       og: {
-        site_name: "Mark2 Analytics",
+        site_name: 'Mark2 Analytics',
         title: nil,
-        description: "",
-        type: "website",
+        description: '',
+        type: 'website',
         url: request.original_url,
-        image: image_url("connected-world-rafiki.png"),
-        locale: "ja_JP",
+        image: image_url('connected-world-rafiki.png'),
+        locale: 'ja_JP',
       },
     }
   end
